5d254f4583255e30b3b6d85d7c97616d26159778
Nishita Ervantikar <120945994+NishitaErvantikar9@users.noreply.github.com>
2023-04-19 23:26:01 +0530

    Delete 1.Band Name generator directory

--- 1.Band Name generator/main.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to The Band Name Generator!")
-city = input("What city did you grow up in?\n")
-pet = input("What is the name of your first pet?\n")
-print("Your band name is " + city +  " " + pet)
